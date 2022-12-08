b09c8682fd7da309ba0d08d48220f41b1e633d64
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2022-12-08 16:56:47 +0100

    first commit from node bases

---
+++ 03-Bases-node/app.js
@@ -0,0 +1,13 @@
+
+console.clear();
+console.log('======================')
+console.log('    Tabla del 5')
+console.log('======================')
+
+const number = 5
+const range = 10
+
+for (let i = 1; i <= range; i++) {
+    const result = i * number;
+    console.log(`${number} X ${i} = ${result}`)
+}
