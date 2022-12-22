a69142f51beef23acdc896401470bc729e24735a
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2022-12-22 12:43:16 +0100

    refactor saveFile.js and fix file is not defined

--- 04-tareas-hacer/helpers/saveFile.js
+++ 04-tareas-hacer/helpers/saveFile.js
@@ -1,34 +1,23 @@
-const fs = require('fs');
+const fs = require("fs");
+const file = "./db/data.json";
 
 const guardarDB = (data) => {
-  const file = './db/data.json';
-
-  // Si data es una array, conviértela a una cadena de texto en formato JSON
-  if (Array.isArray(data)) {
-    data = JSON.stringify(data);
-  }
-
-  fs.writeFileSync(`${file},`, data);
+  fs.writeFileSync(file, JSON.stringify(data));
 };
 
-const leerDB = () =>{
-
-    if(fs.existsSync(file)){
-        return null;
-    }
-
-    const info = fs.readFileSync(file , {encoding: 'utf-8'});
-    console.log(data);
-    
-    const data = JSON.parse(info);
-    
+const leerDB = () => {
+  if (!fs.existsSync(file)) {
     return null;
-    
-    
+  }
 
-}
+  const info = fs.readFileSync(file, { encoding: "utf-8" });
+  const data = JSON.parse(info);
+  console.log(data);
 
+  return null;
+};
 
 module.exports = {
-  guardarDB, leerDB
+  guardarDB,
+  leerDB,
 };
