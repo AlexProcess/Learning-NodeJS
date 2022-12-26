a48b4d95b7715e2cb22ad9072a78080831631eb5
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2022-12-26 21:08:49 +0100

    http server creating excel file with data in this file

--- 06-web-server/app.js
+++ 06-web-server/app.js
@@ -1,11 +1,20 @@
 const http = require('http');
 
 http.createServer( (req, res) => {
+    res.setHeader('Content-Disposition', 'atachment; filename=lista.csv');
+    res.writeHead(200, { 'Content-Type': 'aplication/csv' } )
+
     
-    console.log(req);
+    const persona = {
+        id: 1,
+        name: 'Alex',
+    }   
 
-
-    res.write('Hola mundo')
+    res.write( 'id, nombre\n' )
+    res.write( '1, Fer\n' )
+    res.write( '2, Alex\n' )
+    res.write( '3, Alex\n' )
+    res.write( '4, Alex\n' )
     res.end();
     
 })
