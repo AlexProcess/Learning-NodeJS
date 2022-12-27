8bd3e31e6cc430f453cc17ac499ba2788b23772b
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2022-12-27 22:48:09 +0100

    daily last update

--- 06-web-server/app.js
+++ 06-web-server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 const port = 8080;
 
-// require hbs
+
+
+
+// HandleBars
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 
 // Servir contenido estatico
@@ -20,13 +25,19 @@ app.get('/',  (req, res) => {
   
 });
 
-
 app.get('/generic',  (req, res) => {
-  res.sendFile(__dirname + '/public/generic.html');
+  res.render('generic', {
+    Alex: '',
+    titulo:'Curso node'
+});
 
 });
 app.get('/elements',  (req, res) => {
-  res.sendFile(__dirname + '/public/elements.html');
+  res.render('elements', {
+    Alex: '',
+    titulo:'Curso node'
+});
+
 });
 
 
