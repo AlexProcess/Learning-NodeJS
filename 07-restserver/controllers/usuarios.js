6aeb7f02a9544cdd5f8b940467e1846511bca1e7
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-05 22:06:35 +0100

    routes and controllers ready

---
+++ 07-restserver/controllers/usuarios.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+
+
+
+const usuariosGet = (req, res = response) => {
+    res.json({
+        msg: 'GET api desde el controlador'
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    
+    res.json({
+        msg: 'PUT api desde el controlador'
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    
+    res.json({
+        msg: 'POST api desde el controlador'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    
+    res.json({
+        msg: 'Delete api desde el controlador'
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    
+    res.json({
+        msg: 'Patch api desde el controlador'
+    });
+}
+
+module.exports = {
+    usuariosGet, 
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
