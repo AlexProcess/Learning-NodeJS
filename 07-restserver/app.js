b610b6ff573cffb3811347619df3437dacb16ec3
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-05 15:43:00 +0100

    server ready with enviromment variables

--- 07-restserver/app.js
+++ 07-restserver/app.js
@@ -1,4 +1,12 @@
+require('dotenv').config();
 
+const express = require('express')
+const app = express()
 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
 
-console.log("hello world!");
+app.listen( process.env.PORT, () => {
+    console.log(`Server listening on port`, process.env.PORT)
+});
