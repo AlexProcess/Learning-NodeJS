3c2f1dab3d5c60d860b1441264766bec25a1f3e0
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-08 23:01:49 +0100

    role validated with the BD

--- 07-restserver/routes/usuarios.js
+++ 07-restserver/routes/usuarios.js
@@ -1,5 +1,6 @@
 const { check } = require('express-validator');
 const { Router } = require('express');
+const Role = require('../models/role');
 
 const { usuariosGet,
         usuariosPut,
@@ -8,6 +9,7 @@ const { usuariosGet,
         usuariosPatch } = require('../controllers/usuarios');
 
 const { validarCampos } = require('../middlewares/validar-campos');
+const role = require('../models/role');
 
 
 const router = Router();
@@ -20,7 +22,13 @@ router.post('/', [
    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
    check('password', 'El password es obligatorio y mas de 6 letras').isLength({min: 6}), 
    check('correo', 'El correo no es valido').isEmail(),
-   check('rol', 'No es un rol valido').isIn(["ADMIN_ROLE", "USER_ROLE" ]),
+   //check('rol', 'No es un rol valido').isIn(["ADMIN_ROLE", "USER_ROLE" ]),
+   check("rol").custom( async(rol = "") => {
+      const existeRol = await role.findOne({ rol });
+      if(!existeRol) {
+         throw new Error(`El rol no ${ rol } esta registrado en la base de datos`);
+      }
+   }) ,
    validarCampos
 ],  usuariosPost);
 
