2a55d646bdca9b06fda4542e768905cc6bfa55c1
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-16 16:51:05 +0100

    retornar numero total de registros en una colección

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -8,11 +8,19 @@ const usuario = require("../models/usuario");
 const usuariosGet = async(req = request, res = response) => {
   //const { q, nombre = "noname", apikey, page = 1, limit } = req.query;
   const { limite = 5, desde = 0 } = req.query;
-  const usuarios = await Usuario.find()
-    .skip(Number(desde))
-    .limit(Number(limite))
+  const query = {estado: true}
+
+  //const total = await Usuario.countDocuments(query);
+  
+  const [total, usuarios] = await Promise.all([
+    Usuario.countDocuments(query),
+    Usuario.find(query)
+      .skip(Number( desde ))
+      .limit(Number( limite ))
+  ]);
 
   res.json({
+    total,
     usuarios
   });
 };
