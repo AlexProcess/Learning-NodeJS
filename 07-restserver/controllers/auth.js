0f6cf4f57f02542a93290f6e309f4ac5328fc859
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-18 15:19:51 +0100

    login de usuario

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,13 +1,49 @@
-const { response } = require('express');
+const { response } = require("express");
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response) => {
-  
-  res.json({
-    msg: "login OK",
-  });
+
+const login = async(req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+
+
+    //verificar existencia del email
+    const usuario = await Usuario.findOne({ correo });
+    if ( !usuario ) {
+      return res.status(404).json({
+        msg: 'Usuario / Password no son correctos - Correo'
+      });
+    }
+
+    //Si el usuario esta activo
+    if ( !usuario.estado ) {
+      return res.status(404).json({
+        msg: 'Usuario / Password no son correctos - estado: false'
+      });
+    }
+    //Verificar el password
+    const validPassword = bcryptjs.compareSync( password, usuario.password );
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: 'Usuario / Password no son correctos - Password '
+      })
+    }
+
+    //generar JWT token
+
+    res.json({
+      msg: "login OK",
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Habla con el administrador",
+    });
+  }
 };
 
-
 module.exports = {
-    login
-}
+  login,
+};
