1b097311acd319c41ab745d70f90efea503d6b6c
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-24 19:16:39 +0100

    Crud categorias parte 1

--- 07-restserver/controllers/categoria.js
+++ 07-restserver/controllers/categoria.js
@@ -1,6 +1,44 @@
 const { request, response } = require("express");
+const { populate } = require("../models/categoria");
 const Categoria = require('../models/categoria')
 
+//obtenerCategorias - apginado - total - populate
+const obtenerCategorias = async (req = request, res = response) => {
+
+    const { limite = 5, desde = 0 } = req.query;
+    const query = {estado: true}
+
+  //const total = await Usuario.countDocuments(query);
+
+  const [total, categorias] = await Promise.all([
+    Categoria.countDocuments(query),
+    Categoria.find(query)
+        .populate('usuario', 'nombre' )
+        .skip(Number( desde ))
+        .limit(Number( limite ))
+  ]);
+
+  res.json({
+    total,
+    categorias
+  });
+
+}
+
+//obtenerCategoria populate{}
+const obtenerCategoria = async (req = request, res = response) => {
+    const { id } = req.params;
+    const categoria = await Categoria.findById( id ).populate('usuario', 'nombre');
+
+    return res.json( categoria );
+
+
+}
+
+
+//ActualizarCategoria
+
+//borrar categoria - estado:false 
 
 const crearCategoria = async(req = request, res = response) => {
 
@@ -29,5 +67,7 @@ res.status(201).json(categoria);
 }
 
 module.exports = {
-        crearCategoria
+        crearCategoria,
+        obtenerCategorias,
+        obtenerCategoria,
 }
--- 07-restserver/helpers/db-validators.js
+++ 07-restserver/helpers/db-validators.js
@@ -1,5 +1,5 @@
 const Role = require("../models/role");
-const Usuario = require('../models/usuario');
+const {Usuario, categoria} = require('../models');
 
 const esRoleValido = async (rol = "") => {
   const existeRol = await Role.findOne({ rol });
@@ -24,8 +24,18 @@ const existeUsuarioPorId = async ( id ) => {
   }
 }
 
+const existeCategoriaPorId = async ( id ) => {
+  const existeCategoria = await Usuario.findById( id );
+  if (!existeCategoria) {
+    throw new Error(`El id no existe ${id}`);
+  }
+}
+
+
+
 module.exports = {
     esRoleValido,
     emailExiste,
-    existeUsuarioPorId
+    existeUsuarioPorId,
+    existeCategoriaPorId
 };
