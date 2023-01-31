27a9d6c6c63c5b94e6a6106192b3d557a59ee7ab
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-31 15:39:56 +0100

    validar archivos

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -10,16 +10,32 @@ const cargarArchivo = (req, res = response) => {
     }
   
     const { archivo } = req.files;
-    
-    const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
-  
-    archivo.mv(uploadPath, (err) => {
-      if (err) {
-          return res.status(500).send({ err });
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[ nombreCortado.length - 1 ];
+
+    //Validar la extension
+
+    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
+
+    if ( !extensionesValidas.includes( extension ) ) {
+      return res.status(400).json({
+        msg: `La extension ${extension} no es permitida, ${extensionesValidas}`
+      })
     }
+
+    res.json({
+      extension
+    })
+    
+//     const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
   
-    return res.json({msg: 'File uploaded to ' + uploadPath});
-});
+//     archivo.mv(uploadPath, (err) => {
+//       if (err) {
+//           return res.status(500).send({ err });
+//     }
+  
+//     return res.json({msg: 'File uploaded to ' + uploadPath});
+// });
     
 }
 
