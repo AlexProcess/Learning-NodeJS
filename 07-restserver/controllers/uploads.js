edaed094c6969ffc4858b27ee905af0cb33e7ec8
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-01-31 15:49:17 +0100

    ubicar y cambiar nombre

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,45 +1,40 @@
-const path = require('path');
+const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 const { response } = require("express");
 
-
 const cargarArchivo = (req, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+    res.status(400).send("No files were uploaded.");
+    return;
+  }
 
-    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
-      res.status(400).send('No files were uploaded.');
-      return;
+  const { archivo } = req.files;
+  const nombreCortado = archivo.name.split(".");
+  const extension = nombreCortado[nombreCortado.length - 1];
+
+  //Validar la extension
+
+  const extensionesValidas = ["png", "jpg", "jpeg", "gif"];
+
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extension ${extension} no es permitida, ${extensionesValidas}`,
+    });
+  }
+
+  const nombreTemp= uuidv4() + '.' + extension;
+
+  const uploadPath = path.join(__dirname, "../uploads/", nombreTemp);
+
+  archivo.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).send({ err });
     }
-  
-    const { archivo } = req.files;
-    const nombreCortado = archivo.name.split('.');
-    const extension = nombreCortado[ nombreCortado.length - 1 ];
 
-    //Validar la extension
-
-    const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif']
-
-    if ( !extensionesValidas.includes( extension ) ) {
-      return res.status(400).json({
-        msg: `La extension ${extension} no es permitida, ${extensionesValidas}`
-      })
-    }
-
-    res.json({
-      extension
-    })
-    
-//     const uploadPath = path.join( __dirname, '../uploads/', archivo.name );
-  
-//     archivo.mv(uploadPath, (err) => {
-//       if (err) {
-//           return res.status(500).send({ err });
-//     }
-  
-//     return res.json({msg: 'File uploaded to ' + uploadPath});
-// });
-    
-}
-
+    return res.json({ msg: "File uploaded to " + uploadPath });
+  });
+};
 
 module.exports = {
-    cargarArchivo
-}
+  cargarArchivo,
+};
