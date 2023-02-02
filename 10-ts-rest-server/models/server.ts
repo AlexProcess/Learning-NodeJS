f225b31e3eee5708b221b48a0d2e91fd4a39157a
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-02-02 02:35:35 +0100

    rutas de la app

--- 10-ts-rest-server/models/server.ts
+++ 10-ts-rest-server/models/server.ts
@@ -1,15 +1,24 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/usuario';
 
 
 export class Server {
 
     private app: express.Application;
     private port: string;
-
+    private apiPaths = {
+        usuarios: '/api/usuarios',
+    }
+    
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '8000';
 
+        this.routes();
+    }
+
+    routes() {
+        this.app.use( this.apiPaths.usuarios , userRoutes)
     }
     
     listen() {
